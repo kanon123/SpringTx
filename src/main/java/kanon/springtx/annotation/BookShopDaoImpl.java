da14f69e0766ee697e7e05e66874a6b38a58130b
kanon123 <398920390@qq.com>
2016-06-11 16:32:26 +0800

    update

--- src/main/java/kanon/springtx/annotation/BookShopDaoImpl.java
+++ src/main/java/kanon/springtx/annotation/BookShopDaoImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+
 @Repository("bookShopDao")
 public class BookShopDaoImpl implements BookShopDao {
 
@@ -18,11 +19,11 @@ public class BookShopDaoImpl implements BookShopDao {
 
 	@Override
 	public void updateBookStock(String isbn) {
-		//�����Ŀ���Ƿ��㹻, ������, ���׳��쳣
+		//检查书的库存是否足够, 若不够, 则抛出异常
 		String sql2 = "SELECT stock FROM book_stock WHERE isbn = ?";
 		int stock = jdbcTemplate.queryForObject(sql2, Integer.class, isbn);
 		if(stock == 0){
-			throw new BookStockException("��治��!");
+			throw new BookStockException("库存不足!");
 		}
 		
 		String sql = "UPDATE book_stock SET stock = stock -1 WHERE isbn = ?";
@@ -31,15 +32,14 @@ public class BookShopDaoImpl implements BookShopDao {
 
 	@Override
 	public void updateUserAccount(String username, int price) {
-		//��֤����Ƿ��㹻, ������, ���׳��쳣
+		//验证余额是否足够, 若不足, 则抛出异常
 		String sql2 = "SELECT balance FROM account WHERE username = ?";
 		int balance = jdbcTemplate.queryForObject(sql2, Integer.class, username);
 		if(balance < price){
-			throw new UserAccountException("����!");
+			throw new UserAccountException("余额不足!");
 		}
 		
 		String sql = "UPDATE account SET balance = balance - ? WHERE username = ?";
 		jdbcTemplate.update(sql, price, username);
 	}
-
 }
